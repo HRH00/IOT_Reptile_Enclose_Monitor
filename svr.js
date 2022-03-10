6a2bad863e770fe1cc1c59abfc15e730e1d26c8a
HRH00 <45105662+HRH00@users.noreply.github.com>
2022-03-10 13:35:37 +0000

    Began re-Structuring the project files
    added raspPi gpoi support using node OnOff.js, this turns an Led off or on dependant on the output value of a button.

--- svr.js
+++ svr.js
@@ -1,28 +1,42 @@
 const {readFileSync, writeFileSync} = require ('fs');
 
+
 const express = require('express');
 const app = express();
+//start gpio
+
+//this code creates 2 objects led and button, when the button is pressed, the value of led id set to 0
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(17, 'out');
+const button = new Gpio(4, 'in', 'both');
+button.watch((err, value) => led.writeSync(value));
+//end gpio
 
 app.get('/', (req,res) => {
-    const count = readFileSync('./localStore.txt', 'utf-8');
+    const dbFilePath = './db/localStore.txt';
+    const count = readFileSync(dbFilePath, 'utf-8');
     console.log('count', count);
-
     const newCount = parseInt(count) + 1;
 
-    writeFileSync('./localStore.txt', newCount);
-
+    writeFileSync(dbFilePath, newCount);
     res.send(`
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>RPI Hosted Website</title>
+        <link rel = "stylesheet" type="text/css" href='style.css' >
+        <title>IOT Site</title>
     </head>
     <body>
-        <h1>Website</h1>
+        <h1>IOT Webs App</h1>
         <p>This page has been viewed ${newCount} Times.</p>
     </body>
+    <style>
+        body{
+            background-color:aquamarine;
+        }
+    <\style>
     </html>
     `);
 });
