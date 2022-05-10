3f3551cc21ecb422613a5e778c4e1b38d82ead30
HRH00 <45105662+HRH00@users.noreply.github.com>
2022-05-10 20:32:27 +0100

    Sonic Sensor Added
    Began implementation for the ultrasonic distance sensor

--- svr.js
+++ svr.js
@@ -1,47 +1,121 @@
-const {readFileSync, writeFileSync} = require ('fs');
+const fs = require('fs');
 const dht = require("node-dht-sensor").promises;
 const express = require('express');
-const Gpio = require('onoff').Gpio;
-const BodyParser = require(`body-parser`);
+const Gpio = require('pigpio').Gpio;
 
-//Import our data holding class
+const BodyParser = require(`body-parser`);
+//import npm Webcam 
+const NodeWebcam = require(`node-webcam`);
+const FSWebcam = NodeWebcam.FSWebcam; //require( "node-webcam/webcams/FSWebcam" );
+const opts = {
+  width: 1280,
+  height: 720,
+  quality: 100,
+  frames: 30,
+  saveShots: true,
+  output: "",
+  device: false,
+  callbackReturn: "buffer",
+  verbose: false,
+}
+const cam = new FSWebcam( opts );
+//end webcam init
+
+//Import our data holding classc
 const reptileDataClass = require('./reptileData');
 const { json } = require('express/lib/response');
+const { Console } = require('console');
 
 //express server
 const app = express();
 app.use(express.static('client'));
 app.use(express.static("/client/placeHolder.jpg"));
+app.use(express.static("/client/placeHolder.jpg"));
 app.get('/', (req,res) => {res.send('/client/index.html');});
-app.get('/getData', getData);
-app.get('/lightSwitch',lightSwitch);
-
+//app.get('/getData', getData);
+//app.get('/lightSwitch',lightSwitchB);
+//app.get('/lightSwitch',lightSwitch);
 app.listen(5000, () => console.log('http://localhost:5000/'));
 
-
-
-
-
 const reptileData = new reptileDataClass();
-reptileData.updateAll(0,0,0,0,0,false,0,false)
-updateDataFromSensors();
+reptileData.updateAll(false,0,0,0,0,false,0,false,null);
+//startWaterMonitor();
+
+////takeImage();
+////readFile();
+//
+//const relaya = new Gpio(21, {mode: Gpio.OUTPUT});
+////const relayb = new Gpio(16, {mode: Gpio.OUTPUT});
+//
+//relaya.digitalWrite(0);
+//console.log("Turning on")
+//setTimeout(b, 120000);
+//
+//
+//
+//function b(){
+//  console.log("Turning Off")
+//  relaya.digitalWrite(1);
+//  }
+//
+////Light Control
+//async function lightSwitchB(req,res){
+//  const led = new Gpio(4, {mode: Gpio.OUTPUT});
+//  reptileData.lightOn=!reptileData.lightOn;
+//  led.pwmWrite(255);
+//  if (reptileData.lightOn==true){
+//    console.log("Turning On Light");
+//    let dutyCycle = 0;
+//    const intervalForOn = setInterval(() => {
+//    led.pwmWrite(dutyCycle);
+//    dutyCycle += 5;
+//    if (dutyCycle > 255) {
+//      clearInterval(intervalForOn);
+//    }
+//  }, 20);
+//}
+//
+//  if (reptileData.lightOn==false){
+//    console.log("Turning Off Light");
+//    let dutyCycle = 255;
+//    const intervalForOff = setInterval(() => {
+//      led.pwmWrite(dutyCycle);
+//    dutyCycle -= 5;
+//      if (dutyCycle <= 0) {
+//      clearInterval(intervalForOff);
+//    }
+//  }, 20);
+//  }
+//  res.json(reptileData);
+//}
+//
+//
+//function getLightTargetFromBool(bool){
+//  if (bool === true){
+//    return 255;
+//  }
+//  else{return 0;};
+//}
 
 
-function updateDataFromSensors() {
-  setTimeout(function () {
-    updateDHTA();
-    updateDHTB();
-    updateDataFromSensors();
-    console.log(reptileData);
-  }, 2000);
-}
 
 
-  async function getData(req, res){
-        res.json(reptileData);
-  };
 
 
+
+//function updateDataFromSensors(){
+////remove
+//  setTimeout(function () {
+//  //  updateDHTA();
+////    updateDHTB();
+//    saveFile();
+//    updateDataFromSensors();
+//  }, 5000);
+//}
+//
+//async function getData(req, res){
+//      res.json(reptileData);
+//};
 
 // You can still use the synchronous version of `read`:
 async function updateDHTA(){
@@ -58,6 +132,7 @@ async function updateDHTA(){
     }
   );
 }
+
 async function updateDHTB(){
   dht.setMaxRetries(10);
   dht.initialize(11, 3);
@@ -71,78 +146,84 @@ async function updateDHTB(){
     }
   );
 }
-//we need to do this funciton concurrently - and only update the data
-//for  get request after we have published the sensor reading1 
-//async function updateBothDHT(){
-//  dht.read(11, 2, function(err, temperature, humidity) {
-//    if (!err) {
-//      reptileData.Thermometer1=temperature;
-//      reptileData.Hygrometer1=humidity;
-//      updateDHT2();
-//      return;
-//    }
-//    if (err){throw console.error("Error Reading DHT1 - PIN 2");}
-//  });
-//}
-//async function updateDHT2(){
-//  dht.read(11, 3, function(err, temperature, humidity) {
-//    if (!err) {
-//
-//    
-//      reptileData.Thermometer2=temperature;
-//      reptileData.Hygrometer2=humidity;
-//
-//      return;
-//    }
-//    if (err){throw console.error("Error Reading DHT1");}
-//  });
-//}
 
 
 
 
-////Other Stuff
-//this code creates 2 objects led and button, when the button is pressed, the value of led id set to 0
-async function lightSwitch(req, res){
-  const led = new Gpio(4, 'out');
-  const currentLedValue=reptileData.lightOn;
-  const newLedValue = gpioFlipSwitch(currentLedValue);
-  reptileData.updateLightOn(newLedValue);
-  
-  led.write(newLedValue);
-  res.json(reptileData);}
+
+//global constants
 
 
-function gpioFlipSwitch(start){
-  if (start === 0){
-    return 1;
-  }
-  else{return 0;};
+//image taking and move file loop function
+
+
+function takeImage(){
+  NodeWebcam.capture("picture.tmp", {saveShots: false, callbackReturn: "buffer"}, function(err, data) {
+    //reptileData.Image=(data);// For example use a custom write
+    const datePath = "db/pastImages/Shot-"+Date.now()+".jpg";
+    fs.writeFileSync(datePath, data);
+    // Erase temp file created
+    fs.unlinkSync("picture.tmp");
+});
+
 }
 
-//Start pi-camera
-//const PiCamera = require('pi-camera');
-//const myCamera = new PiCamera({
-//  mode: 'photo',
-//  width: 640,
-//  height: 480,
-//  nopreview: true,
-//});
 
-//myCamera.snapDataUrl()
-///  .then((result) => {
-    // Your picture was captured
-//    console.log('<img src="${result}">');
-//  })
-//  .catch((error) => {
-     // Handle your error
-//  });
-//End pi-camera
-  //const dbFilePath = './db/localStore.txt';
-    //const count = readFileSync(dbFilePath, 'utf-8');
-    //console.log('count', count);
-    //const newCount = parseInt(count) + 1;
+//TODO
+function saveFile(){
+  const dbFilePath = './db/localStore.txt';
+  const data = "\nTime"+Date.now()+" - "+JSON.stringify(reptileData);
+  fs.appendFile(dbFilePath, data ,function(err) {
+    if (err) {console.log(err);}
+  });
+}
+function readFile(){
+  const dbFilePath = './db/localStore.txt';
+  const data = fs.readFile(dbFilePath , 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    parseStringFileToObject(data);
+  });
+}
 
-    //writeFileSync(dbFilePath, newCount);
+function parseStringFileToObject(data){
+  let arrayJson = [];
+  for (let i=0; i<10; i++){
+    arrayJson.push({});
+  }
+  
+  console.log(arrayJson);
+  return arrayJson;
+}
 
 
+
+function startWaterMonitor(){
+  const MICROSECDONDS_PER_CM = 1e6/34321;
+  const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
+  const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+  const waterBowlBottom  = 30//cm from sensor the bottom of the water bowl is
+  trigger.digitalWrite(0); // Make sure trigger is low
+  const watchDistance = () => {
+    let startTick;
+    echo.on('alert', (level, tick) => {
+      if (level == 1) {
+        startTick = tick;
+      } else {
+        const endTick = tick;
+        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+        const distanceMeasured = (diff / 2 / MICROSECDONDS_PER_CM);
+        reptileData.WaterDepth=(waterBowlBottom-distanceMeasured).toFixed(1);
+        console.log(reptileData.WaterDepth);
+      }
+    });
+  };
+  watchDistance();
+  trigger.trigger(10, 1);//initail Trigger
+  // Trigger a distance measurement once per second
+  setInterval(() => {
+    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+  }, 10000);
+}
